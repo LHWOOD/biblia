3e5de25c62791a9bdb9bdc2ddfae4222bdeb29a1
LHWOOD <hutsonwood@gmail.com>
2021-12-04 15:39:06 -0700

    boilerplate and js api fetch

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,26 @@
-var la = "y0";
+const holy = document.querySelector("#btn");
+const here = document.querySelector("#place");
 
-console.log(la);
+const inputHandler = function (event) {
+  event.preventDefault();
+  // let verse = holyInput.value;
+  getHoly();
+};
+
+function getHoly() {
+  let requestUrl = "https://bible-api.com/john 3:16";
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      let verse = document.createElement("p");
+      verse.textContent = data.text;
+      here.appendChild(verse);
+    });
+}
+
+holy.addEventListener("click", inputHandler);
